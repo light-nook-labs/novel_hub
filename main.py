5859d382830e813ee1af8ffa17dbb07cf2060e59
interset-wq <intersetwq@qq.com>
2026-05-01 13:24:48 +0800

    feat: extract data from mult field

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from datetime import datetime, timedelta
-
+import re
 
 if __name__ == "__main__":
+    # date_str = '2026/4/30 16:11:48'
+    # now = datetime.now()
+    # d = datetime.strptime(date_str, "%Y/%m/%d %H:%M:%S")
+    # delta = timedelta(days=30)
+    # print(now, d, delta, sep='\n')
+    # print(now - d < delta)
+    pattern = re.compile(r'(\d+)字\[(.+)\]')
+    s = '50000字[连载中]'
+    print(pattern.search(s).groups())
