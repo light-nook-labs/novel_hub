a0ccf323c7cef3a25204eb3687ee55b317b3244d
interset-wq <intersetwq@qq.com>
2026-05-14 16:39:51 +0800

    feat: use IntEnum instead Enum

--- db.py
+++ db.py
@@ -1,8 +1,18 @@
 from sqlmodel import create_engine, SQLModel
 import models
 
+##########
+# SQLite #
+##########
 
 sqlite_file_name = 'database.db'
 sqlite_url = f'sqlite:///{sqlite_file_name}'
 
-engine = create_engine(sqlite_url, echo=True)
+sqlite_engine = create_engine(sqlite_url, echo=True)
+
+
+############
+# Postgres #
+############
+
+# postgres_engine = create_engine("postgresql://scott:tiger@localhost:5432/mydatabase")
