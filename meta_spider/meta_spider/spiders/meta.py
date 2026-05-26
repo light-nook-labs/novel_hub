c785c9cc781a9dc8bf4a2c5d3796dcabddc89ae9
interset-wq <intersetwq@qq.com>
2026-05-26 16:07:32 +0800

    refactor(meta_spider): rm utils

--- meta_spider/meta_spider/spiders/meta.py
+++ meta_spider/meta_spider/spiders/meta.py
@@ -1,20 +1,21 @@
 from urllib.parse import urlencode, urljoin
 from typing import Any
+import re
+from datetime import datetime
 
 from scrapy import Spider, Request
 from scrapy.exceptions import CloseSpider
 from scrapy.http import HtmlResponse
 
-from meta_spider import utils
 
 class MetaSpider(Spider):
-    # scrapy shell "https://book.sfacg.com/List/default.aspx?PageIndex=1"  
+    # scrapy shell "https://book.sfacg.com/List/default.aspx?PageIndex=1"
     name = "meta"
     allowed_domains = ["book.sfacg.com"]
     # start_urls = ["https://book.sfacg.com"]
     _base_url = "https://book.sfacg.com/List/default.aspx"
     _params = {
-        'PageIndex': 0,
+        "PageIndex": 0,
     }
 
     async def start(self):
@@ -24,49 +25,99 @@ class MetaSpider(Spider):
             yield Request(url, callback=self.parse)
 
     def parse(self, response: HtmlResponse):
-        items = response.css('.Comic_Pic_List')
+        items = response.css(".Comic_Pic_List")
         if not items:
-            raise CloseSpider('No items.')
+            raise CloseSpider("No items.")
         for item in items:
-            novel_url: str | None = item.css('.Conjunction a::attr(href)').get()
-            cover: str | None = item.css('.Conjunction a img::attr(src)').get()
-            title: str | None = item.css('.Conjunction a img::attr(alt)').get()
-            author: str | None  = item.css('a[id*="AuthorLink"]::text').get()
-            score: str | None = item.css('.font_red::text').get()
-            genre: str | None = item.css('.font_red ~a::text').get()
+            novel_url: str | None = item.css(
+                ".Conjunction a::attr(href)"
+            ).get()
+            cover: str | None = item.css(".Conjunction a img::attr(src)").get()
+            title: str | None = item.css(".Conjunction a img::attr(alt)").get()
+            author: str | None = item.css('a[id*="AuthorLink"]::text').get()
+            score: str | None = item.css(".font_red::text").get()
+            genre: str | None = item.css(".font_red ~a::text").get()
             meta_info = dict(
-                nid=int(novel_url.strip('/').split('/')[-1] if novel_url else 0),
+                nid=int(
+                    novel_url.strip("/").split("/")[-1] if novel_url else 0
+                ),
                 cover=cover,
-                title=(title.strip() if title else ''),
-                author=(author.strip() if author else ''),
-                score=float(score.strip().replace('分', '') if score else 5),
-                genre=(genre.strip() if genre else ''),
+                title=(title.strip() if title else ""),
+                author=(author.strip() if author else ""),
+                score=float(score.strip().replace("分", "") if score else 5),
+                genre=(genre.strip() if genre else ""),
             )
             if novel_url:
-                yield response.follow(novel_url, callback=self.parse_detail, cb_kwargs={'meta_info': meta_info})
+                yield response.follow(
+                    novel_url,
+                    callback=self.parse_detail,
+                    cb_kwargs={"meta_info": meta_info},
+                )
         yield response.follow(self._join_url(), callback=self.parse)
 
+    def _row(self, row: list[str]) -> dict[str, int | str | datetime]:
+        # ['类型：魔幻', '字数：3240533字[连载中]', '点击：4757.4万', '更新：2026/5/25 20:26:36']
+        values = [item.split("：")[-1] for item in row]
+        _, wordnum_status, click_num, last_update = values
+        word_num, status = wordnum_status.split("字[")
+        status = status.replace("]", "")
+        return dict(
+            word_num=int(word_num),
+            status=status,
+            click_num=int(
+                click_num
+                if "万" not in click_num
+                else int(click_num.replace("万")) * 10_000
+            ),
+            last_update=datetime.strptime(last_update, "%Y/%m/%d %H:%M:%S"),
+        )
+
+    def _btns(self, btns: list[str]) -> dict[str, int]:
+        # ['点击阅读', '赞 27872', '收藏 278629']
+        # ['赞 294', '收藏 3066']
+        praise_num, like_num = [int(btn.split(" ")[-1]) for btn in btns[-2:]]
+        data = dict(
+            praise_num=praise_num,
+            like_num=like_num,
+        )
+        return data
+
+    def _ptype_contest(self, ptype_contest: list[str]) -> dict[str, str]:
+        # ['VIP', '第九届冬季征文']
+        # ['签约', '2026春季征文']
+        # ['征文大赛长篇']
+        # ['VIP']
+        # []
+        ptypes: set[str] = {"签约", "VIP"}
+        ptype_contest: set[str] = set(ptype_contest)
+        ptype = ptypes & ptype_contest
+        contest = ptype_contest - ptypes
+        return dict(
+            price_type=("免费" if not ptype else ptype.pop()),
+            contest=("" if not contest else contest.pop()),
+        )
+
     def parse_detail(self, response: HtmlResponse, meta_info: dict[str, Any]):
-        row = utils.get_clean_all(response, '.count-detail .text-row .text')
-        btns = utils.get_clean_all(response, '#BasicOperation .btn')
-        title_tags = utils.get_clean_all(response, '.title .tag')    
-        # Cannot use dict(), it doesn't allow the same key appear more than once
-        banner = utils.get_banner(response)
-        yield {
+        row = response.css(".count-detail .text-row .text::text").getall()
+
+        btns = response.css("#BasicOperation .btn::text").getall()
+        # ['点击阅读', '赞 27872', '收藏 278629']
+
+        ptype_contest = response.css(".title .tag::text").getall()
+        # ['VIP', '十一征长篇']
+
+        banner = response.css(".d-banner")
+
+        stags = response.css(".tag-list .tag .highlight .text::text").getall()
+        yield dict(
+            # nid, cover, title, author, score, genre
             **meta_info,
-            **utils.title_tags_parser(title_tags),
-
-            # Cannot change order, 2nd status_id will cover the first one
-            **utils.row_parser(row),
-            **utils.btns_parser(btns),
-
-            # "cover": utils.get_attribute(response, '.summary-pic img'),
-            'has_banner': bool(banner),
-            # ['恋爱', '纯爱', '日常', '女性主角', '变身']
-            # It will be used to create another table
-            'tags': utils.get_clean_all(response, '.tag-list .tag .highlight .text')
-        }
-
+            has_banner=bool(banner),
+            **self._row(row),
+            **self._btns(btns),
+            **self._ptype_contest(ptype_contest),
+            tags=stags,
+        )
 
     def _join_url(self):
         self._params["PageIndex"] += 1
@@ -74,7 +125,3 @@ class MetaSpider(Spider):
             raise CloseSpider()
         params = urlencode(self._params)
         return urljoin(self._base_url, f"?{params}")
-
-
-
-
